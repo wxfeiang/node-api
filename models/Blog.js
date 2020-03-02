b07ccaaea2e72c60a979690f50b56ae68b480f11
wxfeiang <wxfeiang@qq.com>
2020-03-03 00:53:12 +0800

    '添加了个人简历页面的留言接口

--- models/Blog.js
+++ models/Blog.js
@@ -3,28 +3,45 @@ const Schema = mongoose.Schema;
 
 // Create Schema
 const BlogSchema = new Schema({
-  author: {
-    type: String,
-    required: true
-  },
-  content: {
-    type: String,
-    required: true
-  },
-  title: {
-    type: String,
-    required: true
-  },
-  catrgories: [
-    {
-      type: [String],
-      required: true
+    author: {
+        type: String,
+        //required: true
+    },
+    content: {
+        type: String,
+        // required: true
+    },
+    title: {
+        type: String,
+        // required: true
+    },
+    userIP: {
+        type: String,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    // catrgories: [{
+    //     type: [String],
+    //     required: true
+    // }],
+    levmsg: [{
+        text: {
+            type: String,
+            required: true
+        },
+        date: {
+            type: Date,
+            default: Date.now
+        }
+    }],
+
+    date: {
+        type: Date,
+        default: Date.now
     }
-  ],
-  date: {
-    type: Date,
-    default: Date.now
-  }
 });
 
 module.exports = Blog = mongoose.model("blogs", BlogSchema);
--- routes/api/blog.js
+++ routes/api/blog.js
@@ -8,36 +8,74 @@ const validateBlogInput = require("../../validation/blog");
 // @desc   返回的请求的json数据
 // @access public
 router.get("/test", (req, res) => {
-  res.json({ msg: "blog works" });
+    res.json({ msg: "blog works" });
 });
 // $route  GET api/profile/blog
 // @desc   返回的请求的json数据  获取当前登录得用户个人信心
 // @access public
 router.post("/blog", (req, res) => {
-  //console.log(req.body);
-  const { errors, isValid } = validateBlogInput(req.body);
+    //console.log(req.body);
+    const { errors, isValid } = validateBlogInput(req.body);
 
-  // 判断isValid是否通过
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
-  const newBlog = new Blog({
-    author: req.body.author,
-    content: req.body.content,
-    title: req.body.title,
-    catrgories: req.body.catrgories.split(",")
-  });
+    // 判断isValid是否通过
+    // if (!isValid) {
+    //     return res.status(400).json(errors);
+    // }
+    const newBlog = new Blog({
+        author: req.body.author,
+        content: req.body.content,
+        title: req.body.title,
+        catrgories: req.body.catrgories.split(",")
+    });
 
-  newBlog.save().then(blog => res.json(blog));
+    newBlog.save().then(blog => res.json(blog));
 });
-// 获取搜有得数据
-router.get("/blog", (req, res) => {
-  //console.log(req.body);
-  Blog.find()
-    .sort({ date: -1 })
-    .then(posts => res.json(posts))
-    .catch(err => res.status(404).json({ nopostsfound: "找不到任何评论信息" }));
-  //  按时间排序
+router.post("/levmsg", (req, res) => {
+    //console.log(req.body);
+    // const { errors, isValid } = validateBlogInput(req.body);
+
+    // 判断isValid是否通过
+    // if (!isValid) {
+    //     return res.status(400).json(errors);
+    // }
+    // const levmsg = new Blog({
+    //     author: req.body.author,
+    //     content: req.body.content,
+    //     title: req.body.title,
+    //     catrgories: req.body.catrgories.split(",")
+
+    // });
+    const levmsgs = {}
+    if (req.body.author) levmsgs.author = req.body.author;
+    if (req.body.content) levmsgs.content = req.body.content;
+    if (req.body.title) levmsgs.title = req.body.title;
+    if (req.body.userIP) levmsgs.userIP = req.body.userIP;
+    if (req.body.text) levmsgs.text = req.body.text;
+
+
+    new Blog(levmsgs)
+        .save()
+        .then(() => {
+            Blog.find()
+                .sort({ date: -1 })
+                .limit(5)
+                .then(blog => res.json(blog))
+                .catch(err => res.status(404).json({ nopostsfound: "找不到任何评论信息" }));
+
+        })
+
 });
+//  获取所有的 个人简历信息  给前端
+router.get("/levmsg", (req, res) => {
+    //console.log(req.body);
+    Blog.find()
+        .sort({ date: -1 })
+        .limit(5)
+        .then(blog => res.json(blog))
+        .catch(err => res.status(404).json({ nopostsfound: "找不到任何评论信息" }));
+    //  按时间排序
+});
+
+
 
 module.exports = router;
