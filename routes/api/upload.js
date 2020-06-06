8de55ac2dc3a4a4276d0d6cfb7a0b91d636ed275
wxfeiang <wxfeiang@qq.com>
2020-06-06 18:24:11 +0800

    添加文件上传接口

---
+++ routes/api/upload.js
@@ -0,0 +1,80 @@
+const express = require('express')
+const router = express.Router()
+
+const path = require('path') //系统路径模块
+const fs = require('fs') //文件模块
+const multiparty = require('multiparty') //  文件上传插件
+
+// 请求 layui moke table data
+router.get('/layui', (req, res) => {
+  var file = path.join(__dirname, 'json/laui.json')
+  fs.readFile(file, 'utf-8', function (err, data) {
+    if (err) {
+      res.send('文件读取失败')
+    } else {
+      res.send(data)
+    }
+  })
+})
+//  整个文件
+router.get('/layuijson', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'json/index.html'))
+})
+// 请求html    文件
+router.get('/email', (req, res) => {
+  exists = fs.existsSync(__dirname + 'email.html')
+  if (!exists) {
+    fs.mkdirSync(__dirname + 'email.html')
+    console.log('没有文件写进去了')
+  }
+  console.log('有这个文件')
+
+  /**
+  var status = fs.existsSync(`${__dirname}/upload`)
+  if (!status) {
+    // 创建文件
+    fs.mkdir(`${__dirname}/upload`, (error) => {
+      if (error) throw error
+      console.log('目录创建成功。')
+    })
+  } else {
+    console.log('文件夹已经存在')
+  }
+  //  拿到上传的文件数据
+
+  //  写入服务器文件夹
+ */
+})
+//  上传文件
+
+router.post('/upload', (req, res) => {
+  //  fs.exists(path,callback)   检测文件夹是否存在
+  //  注意文件路径 console.log(__dirname)
+  // const upload = multer({ dest: './upload/' }) //  存放文件的地方
+})
+
+router.post('/uploads', (req, res, next) => {
+  var form = new multiparty.Form()
+  form.parse(req, (err, fields, files) => {
+    //  console.log(files) // 上传的文件信息
+
+    var file = files.name[0]
+    console.log(file)
+
+    var rs = fs.createReadStream(file.path)
+    var newPath = '/upload/' + file.originalname
+    var ws = fs.createWriteStream('./public' + newPath)
+    rs.pipe(ws)
+    ws.on('close', (error) => {
+      res.send(newPath)
+    })
+  })
+})
+
+module.exports = router
+/**
+ *
+ * query   ? id ="sss"
+ *
+ * params   '/text/:id/:ff'
+ */
--- server.js
+++ server.js
@@ -1,84 +1,81 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const passport = require('passport');
-const path = require('path'); //  文件路径系统
-const fs = require('fs'); //  文件系统
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const passport = require('passport')
+const path = require('path') //  文件路径系统
+const fs = require('fs') //  文件系统
 //  引入cook  session
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
+var cookieParser = require('cookie-parser')
+var session = require('express-session')
 //  引入静态资源
 
-const app = express();
+const app = express()
 //   各个接口  users/.js
-const users = require('./routes/api/users');
-const profile = require('./routes/api/profile');
-const posts = require('./routes/api/posts');
-const blog = require('./routes/api/blog');
-const wx_jddata = require('./routes/api/wx_jddata');
+const users = require('./routes/api/users')
+const profile = require('./routes/api/profile')
+const posts = require('./routes/api/posts')
+const blog = require('./routes/api/blog')
+const wx_jddata = require('./routes/api/wx_jddata')
+const upload = require('./routes/api/upload') //  文件上传接口
+const acjson = require('./routes/api/acjson') // 请求静态json
+console.log('-----------------------------------')
+console.log('文件根' + __dirname + '---------- 文件名绝对------' + __filename)
 
-
-
-
-const acjson = require('./routes/api/acjson'); // 请求静态json
-console.log('-----------------------------------');
-console.log('文件根' + __dirname + '---------- 文件名绝对------' + __filename);
-
-app.use(express.static('../www/blogadmin/dist')); //  使用静态资源
+app.use(express.static('upload')) //  使用静态资源
 
 //console.log(app.use(express.static(path.join(__dirname, 'public'))));
 //  链接数据库
-var db = require('./config/keys').mongoURI;
+var db = require('./config/keys').mongoURI
 //  已经在服务器配置好了生产环境得变量   product
 if (process.env.NODE_ENV === 'production') {
-    console.log('线上生产环境 ');
-    db = require('./config/keys').mongoURIProt;
+  console.log('线上生产环境 ')
+  db = require('./config/keys').mongoURIProt
 }
 mongoose
-    .connect(db, {
-        useNewUrlParser: true
-    })
-    .then(() => console.log(process.env.NODE_ENV + '数据库链接成功'))
-    .catch(err => console.log(err));
+  .connect(db, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log(process.env.NODE_ENV + '数据库链接成功'))
+  .catch((err) => console.log(err))
 // Connect to mongodb
 
 // bodyParser 中间件使用
 app.use(
-    bodyParser.urlencoded({
-        extended: false
-    })
-);
-app.use(bodyParser.json());
+  bodyParser.urlencoded({
+    extended: false,
+  })
+)
+app.use(bodyParser.json())
 
 // 使用中间件实现允许跨域
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
-    next();
-});
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS')
+  next()
+})
 //  初始化   session
-app.use(cookieParser('sessiontest'));
+app.use(cookieParser('sessiontest'))
 // 使用 session 中间件
 app.use(
-    session({
-        secret: 'sessiontest', // 对session id 相关的cookie 进行签名 与cookieParser中的一致
-        resave: true,
-        saveUninitialized: false, // 是否保存未初始化的会话
-        cookie: {
-            maxAge: 5000 // 设置 session 的有效时间，单位毫秒
-        }
-    })
-);
+  session({
+    secret: 'sessiontest', // 对session id 相关的cookie 进行签名 与cookieParser中的一致
+    resave: true,
+    saveUninitialized: false, // 是否保存未初始化的会话
+    cookie: {
+      maxAge: 50000, // 设置 session 的有效时间，单位毫秒
+    },
+  })
+)
 
 // 初始化
-app.use(passport.initialize());
-require('./config/passport')(passport); // 数据分离
+app.use(passport.initialize())
+require('./config/passport')(passport) // 数据分离
 
 //根路径
 app.get('/', (req, res) => {
-    res.send('这里是项目跟路径'); //
-});
+  res.send('这里是项目跟路径') //
+})
 
 // app.get('/', (req, res) => {
 //     res.sendFile(
@@ -87,16 +84,15 @@ app.get('/', (req, res) => {
 // });
 // 读取整个文件夹
 
-app.use('/api/users', users); // 上面引入进来的
-app.use('/api/profile', profile);
-app.use('/api/posts', posts);
-app.use('/api/blog', blog);
-app.use('/api/acjson', acjson);
-app.use('/api/wx_jddata', wx_jddata);
+app.use('/api/users', users) // 上面引入进来的
+app.use('/api/profile', profile)
+app.use('/api/posts', posts)
+app.use('/api/blog', blog)
+app.use('/api/acjson', acjson)
+app.use('/api/wx_jddata', wx_jddata)
+app.use('/api/upload', upload)
 
-
-
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
 app.listen(port, () => {
-    console.log(`Server  running  on prot  ${port}`);
-});
+  console.log(`Server  running  on prot  ${port}`)
+})
