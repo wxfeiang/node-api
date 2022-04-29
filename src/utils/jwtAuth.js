f0d216bd98f94ea1177d47a244f8571eca50f0dd
wxfeiang <wxfeiang@qq.com>
2022-04-29 22:20:20 +0800

    添加了 小程序token验证，自定义解析Token

--- src/utils/jwtAuth.js
+++ src/utils/jwtAuth.js
@@ -6,7 +6,17 @@ const { jwtConfig } = require('../config/config')
 const authJwt = expressJwt({
   secret: jwtConfig.secretOrKey, //加密密钥，可换
   algorithms: ['HS256'], //
-  credentialsRequired: true // 校验
+  credentialsRequired: true, // 校验
+  getToken: function fromHeaderOrQuerystring(req) {
+    if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+      return req.headers.authorization.split(' ')[1]
+    } else if (req.query && req.query.token) {
+      return req.query.token
+    } else if (req.headers['access-token'] && req.headers['access-token'].split(' ')[0] === 'Bearer') {
+      return req.headers['access-token'].split(' ')[1]
+    }
+    return null
+  }
 }).unless({
   // path: [/^\/api/] //添加不需要token验证的路由  api开头的不需要验证
   path: ['/api/users/login', '/api/users/multer', '/api/jkgs/jkgsAdduser'] //添加不需要token验证的路由
