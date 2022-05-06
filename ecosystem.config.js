5f3a05bf8e4efac16bdef1c3160e23d0c93875ff
wxfeiang <wxfeiang@qq.com>
2022-05-06 17:08:03 +0800

    gegnxin pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -29,7 +29,7 @@ module.exports = {
       ref: 'origin/node2',
       repo: 'https://github.com/wxfeiang/node-api.git',
       path: '/home/node2', // path 指定项目目录
-      'pre-deploy': 'git fetch --all',
+      //'pre-deploy': 'git fetch --all',
       // Pre-setup 在 setup 之前执行，如安装 git
       // 'pre-setup': 'apt-get install git ; ls -la',
       // Post-setup 在 setup 之后执行
