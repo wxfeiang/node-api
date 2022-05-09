5ddfa35078967c7bda74b3483fd95655509e2ac4
wxfeiang <wxfeiang@qq.com>
2022-05-09 21:34:41 +0800

    prot

--- src/config/swaggers.js
+++ src/config/swaggers.js
@@ -5,6 +5,7 @@ const tool = require('../utils/tool')
 const IP = tool.getIpAddress()
 
 const currentIP = process.env.HOST || 'localhost'
+const swagerRort = process.env.HOST ? '80' : process.env.PORT
 
 console.log(currentIP, '---')
 
@@ -15,7 +16,7 @@ let options = {
       title: 'node-api',
       version: '1.0.0'
     },
-    host: currentIP + ':' + process.env.PORT, //端口号，要和自己启动的node服务保持一致 // 或者localhost  '127.0.0.1:' +
+    host: currentIP + ':' + swagerRort, //端口号，要和自己启动的node服务保持一致 // 或者localhost  '127.0.0.1:' +
     basePath: '/',
     produces: ['application/json', 'application/xml'],
     schemes: ['http', 'https'],
